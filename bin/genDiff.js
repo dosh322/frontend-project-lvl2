1c80790e7bf805a7f95983e57bc9d8b16b90921d
Andrei Prokofev <palych322@gmail.com>
2020-10-31 15:39:54 +0300

    add format opt & argList

--- bin/genDiff.js
+++ bin/genDiff.js
@@ -1,3 +1,13 @@
 #!/usr/bin/env node
+import program from 'commander';
 
-console.log('Check working');
+program
+  .description('Compares two configuration files and shows a difference.')
+  .version('0.1.0')
+  .option('-f, --format [type]', 'output format')
+  .arguments('<filepath1> <filepath2>');
+
+program.parse(process.argv);
+
+if (program.format === true) console.log('format');
+else console.log(`check ${program.format}`);
