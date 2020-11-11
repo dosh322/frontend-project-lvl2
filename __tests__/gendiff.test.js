900a912ee40e3952c6d755afd7cce7e044665891
Andrei Prokofev <palych322@gmail.com>
2020-11-11 15:23:28 +0300

    upd tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -10,20 +10,25 @@ const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-const result = readFile('result-json.txt');
+const resultStylish = readFile('result-stylish.txt');
+const resultPlain = readFile('result-plain.txt');
 
 test('JSON test', () => {
   const beforeJson = getFixturePath('before.json');
   const afterJson = getFixturePath('after.json');
 
-  const diff = genDiff(beforeJson, afterJson);
-  expect(diff).toEqual(result);
+  const diffStylish = genDiff(beforeJson, afterJson, 'stylish');
+  const diffPlain = genDiff(beforeJson, afterJson, 'plain');
+  expect(diffStylish).toEqual(resultStylish);
+  expect(diffPlain).toEqual(resultPlain);
 });
 
 test('yml test', () => {
   const beforeYml = getFixturePath('before.yml');
   const afterYml = getFixturePath('after.yml');
 
-  const diff = genDiff(beforeYml, afterYml);
-  expect(diff).toEqual(result);
+  const diffStylish = genDiff(beforeYml, afterYml, 'stylish');
+  const diffPlain = genDiff(beforeYml, afterYml, 'plain');
+  expect(diffStylish).toEqual(resultStylish);
+  expect(diffPlain).toEqual(resultPlain);
 });
