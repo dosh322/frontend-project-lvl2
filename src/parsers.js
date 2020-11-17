91232e9631c8ffaf9cecb2f9d263f4acc8067e0d
Andrei Prokofev <palych322@gmail.com>
2020-11-17 14:45:40 +0300

    parsers refactoring

--- src/parsers.js
+++ src/parsers.js
@@ -1,14 +1,8 @@
 import yaml from 'js-yaml';
 
-const parse = (fileContent, dataFormat) => {
-  switch (dataFormat) {
-    case 'json':
-      return JSON.parse(fileContent);
-    case 'yml':
-      return yaml.safeLoad(fileContent);
-    default:
-      throw new Error(`Unexpected data format: ${dataFormat}`);
-  }
+const parsers = {
+  json: JSON.parse,
+  yml: yaml.safeLoad,
 };
 
-export default parse;
+export default (fileContent, dataFormat) => parsers[dataFormat](fileContent);
